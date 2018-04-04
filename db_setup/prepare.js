a2cfe10d44eca8264e7ed86e797641c44c939c05
Jackson Pires <jackson.pires@gmail.com>
2018-04-03 22:01:56 -0300

    Update prepare.js

--- db_setup/prepare.js
+++ db_setup/prepare.js
@@ -7,9 +7,9 @@ var conString = database.conString;
 var client = new pg.Client(conString);
 client.connect();
 
-//client.query("CREATE TABLE todos(id serial not null primary key, text name, done boolean)");
-//client.query("INSERT INTO todos(text, done) values('Hi!',true)");
-//client.query("INSERT INTO todos(text, done) values('Hello!', false)");
+client.query("CREATE TABLE todos(id serial not null primary key, text name, done boolean)");
+client.query("INSERT INTO todos(text, done) values('Hi!',true)");
+client.query("INSERT INTO todos(text, done) values('Hello!', false)");
 
 var query = client.query("SELECT * FROM todos");
 
